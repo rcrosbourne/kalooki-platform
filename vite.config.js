f7f08ed86be0fe309d90ce9a2ff0b00e90659098
Rainaldo Crosbourne <rcrosbourne@Rainaldos-MacBook-Pro.local>
2022-10-02 05:53:06 -0500

    Intial setup with vite ddev websockets

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: 'resources/js/app.jsx',
+            ssr: 'resources/js/ssr.jsx',
+            refresh: true,
+        }),
+        react(),
+    ],
+    ssr: {
+        noExternal: ['@inertiajs/server'],
+    },
+    server: {
+        hmr: {
+            protocol: 'wss',
+            host: 'kalooki-platform.ddev.site',
+        },
+        host: '0.0.0.0',
+        strictPort: true,
+        port: 3000,
+    }
+});
