d4d178a7adc49880811d8b02208ae336b944d11e
Rainaldo Crosbourne <rcrosbourne@Rainaldos-MacBook-Pro.local>
2022-10-15 23:23:52 -0500

    Added draggable and droppable. You can now sort your hand

--- resources/js/Components/Card.jsx
+++ resources/js/Components/Card.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {createStyles} from "@mantine/core";
+import {Draggable} from "react-beautiful-dnd";
 
 const useStyles = createStyles((theme) => ({
     card: {
@@ -38,13 +39,17 @@ const useStyles = createStyles((theme) => ({
         justifyContent: "center",
         background: "radial-gradient(50% 50% at 50% 50%, #D3A9F4 0%, #32373C 100%)",
         border: "1px solid #000000"
-    }
+    },
+    cardDragging: {
+        boxShadow: theme.shadows.sm,
+        border: `1px solid ${theme.colors.red[5]}`,
+    },
 
 }));
 const SUITS = {"spades": '♠', "clubs": '♣', "hearts": '♥', "diamond": '♦'};
 const VALUES = {"ace": 'A', "jack": 'J', "queen": 'Q', "king": 'K', "10": 10, "9": 9, "8": 8, "7": 7, "6": 6, "5": 5, "4": 4, "3": 3, "2": 2};
 const JOKER = {"value": "joker", "suit": "👻️"};
-export default function Card({suit, value, faceUp}) {
+export default function Card({suit, value, faceUp, index}) {
     const {classes, cx} = useStyles();
     if (!faceUp) {
         return (
@@ -52,15 +57,31 @@ export default function Card({suit, value, faceUp}) {
         );
     }
     if (value === "joker") {
-        return (<div className={cx(classes.joker)}>
-            <span>{JOKER.suit}</span>
-        </div>);
+        return (
+            <Draggable key={value + suit} index={index} draggableId={value + suit}>
+                {(provided, snapshot) => (
+                    <div className={cx(classes.joker, {[classes.cardDragging]: snapshot.isDragging})}
+                         {...provided.draggableProps}
+                         {...provided.dragHandleProps}
+                         ref={provided.innerRef}>
+                        <span>{JOKER.suit}</span>
+                    </div>
+                )}
+            </Draggable>
+        );
     }
     return (
-        <div className={cx(classes.card)}>
-            <div className={cx(classes.faceUp)}>
-                <span>{VALUES[value]}</span><span>{SUITS[suit]}</span>
-            </div>
-        </div>
+        <Draggable key={value + suit} index={index} draggableId={value + suit}>
+            {(provided, snapshot) => (
+                <div className={cx(classes.card, {[classes.cardDragging]: snapshot.isDragging})}
+                     {...provided.draggableProps}
+                     {...provided.dragHandleProps}
+                     ref={provided.innerRef}>
+                    <div className={cx(classes.faceUp)}>
+                        <span>{VALUES[value]}</span><span>{SUITS[suit]}</span>
+                    </div>
+                </div>
+            )}
+        </Draggable>
     );
 }
--- resources/js/Pages/Board.jsx
+++ resources/js/Pages/Board.jsx
@@ -1,8 +1,22 @@
 import {Link, Head} from '@inertiajs/inertia-react';
 import React, {useEffect, useState} from 'react';
 import Card from "@/Components/Card";
+import {DragDropContext, Droppable} from "react-beautiful-dnd";
+import {useListState} from "@mantine/hooks";
 
+const cards = [
+    <Card suit={"diamond"} value={"king"} faceUp={true} index={0} key={0}/>,
+    <Card suit={"diamond"} value={"jack"} faceUp={true} index={1} key={1}/>,
+    <Card value={"joker"} faceUp={true} index={2} key={2}/>,
+    <Card value={"queen"} suit={"hearts"} faceUp={true} index={3} key={3}/>,
+    <Card value={"ace"} suit={"spades"} faceUp={true} index={4} key={4}/>,
+    <Card value={"2"} suit={"spades"} faceUp={true} index={5} key={5}/>,
+    <Card value={"3"} suit={"spades"} faceUp={true} index={6} key={6}/>,
+    <Card value={"4"} suit={"spades"} faceUp={true} index={7} key={7}/>,
+    <Card value={"5"} suit={"spades"} faceUp={true} index={8} key={8}/>,
+];
 export default function Board() {
+    const [playerHand, playerHandHandler] = useListState(cards);
     return (
         <>
             <Head title="Board"/>
@@ -13,34 +27,33 @@ export default function Board() {
                         <h3>Turn: Nina</h3>
                     </div>
                 </header>
-                <div className="flex-1 w-full mt-[30px] bg-[#27577B] border border-[#32373C] rounded-[10px] grid grid-cols-4 max-h-[433px] gap-2 p-4">
-                    <div>Opponent Top Three</div>
-                    <div className="col-start-4">Opponent four</div>
-                    <div className="">Opponent Bottom Three</div>
-                    <div className="col-start-2 row-start-3">Stock Pile</div>
-                    <div className="col-start-3 row-start-3">Discard Pile</div>
-                    <div className="row-start-4">Player Top Three</div>
-                    <div className="row-start-5">Player Bottom Three</div>
-                    <div className="row-start-5 col-start-4">Player Four</div>
-                </div>
-                <div>
-                    <menu className="w-full space-x-2 flex items-center justify-between">
-                        <button className="bg-[#12B886] text-[#CBD6E1] font-bold py-[10px] px-[20px] rounded-[3px] mt-[30px] uppercase">Draw</button>
-                        <button className="bg-[#12B886] text-[#CBD6E1] font-bold py-[10px] px-[20px] rounded-[3px] mt-[30px] uppercase">Discard</button>
-                        <button className="bg-[#12B886] text-[#CBD6E1] font-bold py-[10px] px-[20px] rounded-[3px] mt-[30px] uppercase">Lay Cards</button>
-                    </menu>
-                    <div className="mt-[20px] border-2 border-pink-500 p-4 rounded text-gray-50 flex -space-x-4">
-                        <Card suit={"diamond"} value={"king"} faceUp={true} />
-                        <Card suit={"diamond"} value={"jack"} faceUp={true} />
-                        <Card value={"joker"} faceUp={true} />
-                        <Card value={"queen"} suit={"hearts"} faceUp={true} />
-                        <Card value={"ace"} suit={"spades"} faceUp={true} />
-                        <Card value={"2"} suit={"spades"} faceUp={true} />
-                        <Card value={"3"} suit={"spades"} faceUp={true} />
-                        <Card value={"4"} suit={"spades"} faceUp={true} />
-                        <Card value={"5"} suit={"spades"} faceUp={true} />
+                <DragDropContext onDragEnd={({source, destination}) => playerHandHandler.reorder({from: source.index, to: destination.index})}>
+                    <div className="flex-1 w-full mt-[30px] bg-[#27577B] border border-[#32373C] rounded-[10px] grid grid-cols-4 max-h-[433px] gap-2 p-4">
+                        <div>Opponent Top Three</div>
+                        <div className="col-start-4">Opponent four</div>
+                        <div className="">Opponent Bottom Three</div>
+                        <div className="col-start-2 row-start-3">Stock Pile</div>
+                        <div className="col-start-3 row-start-3">Discard Pile</div>
+                        <div className="row-start-4">Player Top Three</div>
+                        <div className="row-start-5">Player Bottom Three</div>
+                        <div className="row-start-5 col-start-4">Player Four</div>
                     </div>
-                </div>
+                    <div>
+                        <menu className="w-full space-x-2 flex items-center justify-between">
+                            <button className="bg-[#12B886] text-[#CBD6E1] font-bold py-[10px] px-[20px] rounded-[3px] mt-[30px] uppercase">Draw</button>
+                            <button className="bg-[#12B886] text-[#CBD6E1] font-bold py-[10px] px-[20px] rounded-[3px] mt-[30px] uppercase">Discard</button>
+                            <button className="bg-[#12B886] text-[#CBD6E1] font-bold py-[10px] px-[20px] rounded-[3px] mt-[30px] uppercase">Lay Cards</button>
+                        </menu>
+                        <Droppable droppableId="player-hand" direction="horizontal">
+                            {(provided) => (
+                                <div {...provided.droppableProps} ref={provided.innerRef} className="mt-[20px] border-2 border-pink-500 p-4 rounded text-gray-50 flex -space-x-4">
+                                    {playerHand}
+                                    {provided.placeholder}
+                                </div>
+                            )}
+                        </Droppable>
+                    </div>
+                </DragDropContext>
             </div>
         </>
     );
