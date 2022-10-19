c0a5403c2da2be5d6ba66f50652d51e1b0d3c115
Rainaldo Crosbourne <rcrosbourne@Rainaldos-MacBook-Pro.local>
2022-10-19 10:32:38 -0500

    Custom meld spacing

--- resources/js/Components/Meld.tsx
+++ resources/js/Components/Meld.tsx
@@ -7,13 +7,20 @@ interface Props {
 
 }
 export default function Meld({ droppableId, cards, className}: Props) {
+    const getMeldSpacing = () => {
+        if(cards.length <= 4) {
+            return "-space-x-6 "
+        } else {
+            return "-space-x-8 "
+        }
+    }
     return (
         <Droppable droppableId={droppableId} direction="horizontal">
             {(provided) => (
                 <div
                     {...provided.droppableProps}
                     ref={provided.innerRef}
-                    className={"flex -space-x-6 " + className}>
+                    className={"flex " + getMeldSpacing() + className}>
                     {cards.map((card, index) => (
                         <div key={index} className="flex-1">
                             {card}
