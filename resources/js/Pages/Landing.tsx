36054f13be7c0c2296c00e15668fac95996e74cb
Rainaldo Crosbourne <rcrosbourne@Rainaldos-MacBook-Pro.local>
2022-11-02 10:01:44 -0500

    Removed login and start new trial

--- resources/js/Pages/Landing.tsx
+++ resources/js/Pages/Landing.tsx
@@ -44,17 +44,6 @@ export default function Landing(props) {
                   ))}
                 </div>
               </div>
-              <div className="hidden md:flex md:items-center md:space-x-6">
-                <a href="#" className="text-base font-medium text-white hover:text-gray-300">
-                  Log in
-                </a>
-                <a
-                  href="#"
-                  className="inline-flex items-center rounded-md border border-transparent bg-gray-600 px-4 py-2 text-base font-medium text-white hover:bg-gray-700"
-                >
-                  Start free trial
-                </a>
-              </div>
             </nav>
           </div>
 
